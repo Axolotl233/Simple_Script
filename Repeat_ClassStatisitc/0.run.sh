e8e05b201c0f6304277b3e8e4d7fb7cde9bc1c39
Axolotl233 <45548556+Axolotl233@users.noreply.github.com>
2020-05-28 00:09:08 +0800

    Delete 0.run.sh

--- Repeat_ClassStatisitc/0.run.sh
+++
@@ -1,7 +0,0 @@
-perl Util/TypeBed.pl > All.repeat.type.sort.bed
-perl Util/SplitBed.pl All.repeat.type.sort.bed
-perl Util/CreatRepeatChart.pl $theadnum > RepeatStatistic.txt
-perl Util/RateRepeat.pl $genome_size RepeatStatistic.txt
-perl Util/SplitGff3.pl $PathIncludeRepeatGFF
-perl Util/ClassStatisticByContig.pl $threadnum
-perl Util/MergeClassRes.pl $genome_size $repeatsize
